3d96e7ed69b91ed7a94ef796823cf2d4b24c9c86
pedester321 <pedro.castro.95@outlook.com>
2024-09-19 15:22:33 -0300

    Added tracing to auth-api

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,26 +1,26 @@
-import express from "express"
+import express from "express";
 
 import userRoutes from "./scr/modules/user/routes/UserRoutes.js";
-import checkToken from "./scr/config/auth/checkToken.js";
+import tracing from "./scr/config/tracing.js";
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
 
 app.use(express.json());
-
+app.use(tracing);
 app.use(userRoutes);
 
 //app.use(checkToken)
 
-app.get('/api/status', (req, res) => {
-    return res.status(200).json({
-        servise: 'Auth-API',
-        status: 'up',
-        httpStatus: 200,
-    })
+app.get("/api/status", (req, res) => {
+  return res.status(200).json({
+    servise: "Auth-API",
+    status: "up",
+    httpStatus: 200,
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started successfully at port ${PORT}`)
+  console.log(`Server started successfully at port ${PORT}`);
 });
--- auth-api/scr/modules/user/service/UserService.js
+++ auth-api/scr/modules/user/service/UserService.js
@@ -2,90 +2,112 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 import UserRepository from "../repository/userRepository.js";
-import * as httpStatus from "../../../config/constants/httpStatus.js"
+import * as httpStatus from "../../../config/constants/httpStatus.js";
 import UserException from "../exception/UserException.js";
-import * as secrets from "../../../config/constants/secrets.js"
-
+import * as secrets from "../../../config/constants/secrets.js";
+import { response } from "express";
 
 class UserService {
+  async findByEmail(req) {
+    try {
+      const { email } = req.params;
+      const { authUser } = req;
+      this.validateRequestData(email);
+      let user = await UserRepository.findByEmail(email);
+      this.validateUserNotFound(user);
+      this.validateAuthenticatedUser(user, authUser);
+      return {
+        status: httpStatus.SUCCESS,
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        },
+      };
+    } catch (err) {
+      return {
+        status: err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR,
+        message: err.message,
+      };
+    }
+  }
 
-    async findByEmail(req){
-        try {
-            const {email} = req.params
-            const {authUser} = req
-            this.validateRequestData(email)
-            let user = await UserRepository.findByEmail(email)
-            this.validateUserNotFound(user)
-            this.validateAuthenticatedUser(user, authUser)
-            return{
-                status: httpStatus.SUCCESS,
-                user: {
-                    id: user.id,
-                    name: user.name,
-                    email: user.email,
-                },
-            }
+  async getAccessToken(req) {
+    try {
+      const { transactionid, serviceid } = req.headers;
+      console.log(
+        `Request to POST login with data ${JSON.stringify(
+          req.body
+        )} | [transactionID]: ${transactionid} | serviceID: ${serviceid}`
+      );
+      const { email, password } = req.body;
+      this.validateAccessTokenData(email, password);
+      let user = await UserRepository.findByEmail(email);
+      this.validateUserNotFound(user);
+      await this.validatePassword(password, user.password);
+      const authUser = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      };
+      const accessToken = jwt.sign({ authUser }, secrets.API_SECRET, {
+        expiresIn: "1d",
+      });
+      let response = {
+        status: httpStatus.SUCCESS,
+        accessToken,
+      };
+      console.log(
+        `Response to POST login with data ${JSON.stringify(
+          response
+        )} | [transactionID]: ${transactionid} | serviceID: ${serviceid}`
+      );
+      return response;
+    } catch (err) {
+      return {
+        status: err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR,
+        message: err.message,
+      };
+    }
+  }
 
-        } catch (err) {
-            return {
-                status: err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR,
-                message: err.message,
-            }
-        }
-
+  validateRequestData(email) {
+    if (!email) {
+      throw new UserException(
+        httpStatus.BAD_REQUEST,
+        "User email was not informed."
+      );
     }
-
-    async getAccessToken(req){
-        try {
-            const {email, password} = req.body
-            this.validateAccessTokenData(email, password)
-            let user = await UserRepository.findByEmail(email)
-            this.validateUserNotFound(user)
-            await this.validatePassword(password, user.password)
-            const authUser = {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-            }
-            const accessToken = jwt.sign({authUser}, secrets.API_SECRET,{expiresIn: "1d"})
-            return {
-                status: httpStatus.SUCCESS,
-                accessToken,
-            }
-        } catch (err) {
-            return {
-                status: err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR,
-                message: err.message,
-            }
-        }
-
+  }
+  validateUserNotFound(user) {
+    if (!user) {
+      throw new UserException(
+        httpStatus.BAD_REQUEST,
+        "User user was not found."
+      );
     }
-
-    validateRequestData(email){
-        if(!email){
-            throw new UserException(httpStatus.BAD_REQUEST,'User email was not informed.');
-        }
+  }
+  validateAccessTokenData(email, password) {
+    if (!email || !password) {
+      throw new UserException(
+        httpStatus.UNAUTHORIZED,
+        "Email and password must be informed."
+      );
     }
-    validateUserNotFound(user){
-        if(!user){
-            throw new UserException(httpStatus.BAD_REQUEST,'User user was not found.');
-        }
+  }
+  async validatePassword(password, hashPassword) {
+    if (!(await bcrypt.compare(password, hashPassword))) {
+      throw new UserException(httpStatus.UNAUTHORIZED, "Incorrect password.");
     }
-    validateAccessTokenData(email, password){
-        if(!email || !password){
-            throw new UserException(httpStatus.UNAUTHORIZED, "Email and password must be informed.")
-        }
+  }
+  validateAuthenticatedUser(user, authUser) {
+    if (!authUser || user.id !== authUser.id) {
+      throw new UserException(
+        httpStatus.FORBIDDEN,
+        "You cannot see this user data."
+      );
     }
-    async validatePassword(password, hashPassword){
-        if(!await bcrypt.compare(password, hashPassword)){
-            throw new UserException(httpStatus.UNAUTHORIZED, "Incorrect password.")
-        }
-    }
-    validateAuthenticatedUser(user, authUser){
-        if(!authUser || (user.id !== authUser.id)){
-            throw new UserException(httpStatus.FORBIDDEN, "You cannot see this user data.")
-        }
-    }
+  }
 }
 
 export default new UserService();
