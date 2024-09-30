d7037c853545714f9aa560eb53b28526692069b9
pedester321 <pedro.castro.95@outlook.com>
2024-09-30 15:46:23 -0300

    fixed UserServiceBug

--- auth-api/scr/modules/user/controller/UserController.js
+++ auth-api/scr/modules/user/controller/UserController.js
@@ -1,16 +1,15 @@
-import UserService from "../service/userService.js";
+import UserService from "../service/UserService.js";
 
-class UserController{
+class UserController {
+  async getAccessToken(req, res) {
+    let accessToken = await UserService.getAccessToken(req);
+    return res.status(accessToken.status).json(accessToken);
+  }
 
-    async getAccessToken (req, res){
-        let accessToken = await UserService.getAccessToken(req)
-        return res.status(accessToken.status).json(accessToken)
-    }
-
-    async findByEmail(req, res){
-        let user = await UserService.findByEmail(req)
-        return res.status(user.status).json(user);
-    }
+  async findByEmail(req, res) {
+    let user = await UserService.findByEmail(req);
+    return res.status(user.status).json(user);
+  }
 }
 
 export default new UserController();
